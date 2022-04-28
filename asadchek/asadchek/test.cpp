07363a38f067c42326fb77dbc3164e9f2e2fb63a
ElbaErk <ac2295795@akfauniversity.org>
2022-04-28 13:28:15 +0500

    test added

---
+++ asadchek/asadchek/test.cpp
@@ -0,0 +1,8 @@
+//
+//  test.cpp
+//  asadchek
+//
+//  Created by ELBEK ERKINBOEV on 28/04/22.
+//
+
+#include "test.hpp"
---
+++ asadchek/asadchek/test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  asadchek
+//
+//  Created by ELBEK ERKINBOEV on 28/04/22.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
