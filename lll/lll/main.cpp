a87f852126dc5589f9d38416a25322f41ae4c91c
ASADBEK FAZLIDDINOV <asadbekfazlid7747@gmail.com>
2022-04-28 13:06:31 +0500

    kk
    ll

---
+++ lll/lll/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lll
+//
+//  Created by ASADBEK FAZLIDDINOV on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
